e9a591e19e9355eb69eb9e60a3330922d43aa132
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-03-31 17:10:39 -0300

    acabei, mas não ta funcionando

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calcular() {
-    let peso = (document.getElementById("peso").value);
-    let altura = (document.getElementById("altura").value);
+    let peso = parseFloat(document.getElementById("peso").value);
+    let altura = parseFloat(document.getElementById("altura").value);
     let resultado = document.getElementById("resultado");
     
 }
@@ -10,6 +10,23 @@ if (isNaN(peso) || isNaN(altura) || altura <= 0) {
     resultado.styles.color = "red";
     return;
 }
-let imc = peso / (altura ** 2);
 
-let mensagem = `Seu IMC é: ${imc.toFixed(1)}<br>`
+let imc = peso / (altura ** 2);
+let mensagem = `Seu IMC é: ${imc.toFixed(1)}<br>`;
+
+if (imc < 18.5) {
+    mensagem += "Você está Abaixo do Peso Ideal";
+} else if (imc < 25) {
+    mensagem += "Você está no Peso Ideal";
+} else if (imc < 30) {
+    mensagem += "Você está Levemente Acima do Peso Ideal";
+} else if (imc < 35) {
+    mensagem += "Você está com Obesidade";
+} else if (imc < 40) {
+    mensagem += "Você está com Obesidade Severa";
+} else {
+    mensagem += "Você está com Obesidade Mórbida";
+}
+
+resultado.innerHTML = mensagem;
+resultado.styles.color = "black";
