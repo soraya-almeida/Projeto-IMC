59e773c6d91cbd0e85cd73227d9572a5df18d34e
Soraya Almeida <samiraalmeida4169@gmail.com>
2025-03-31 16:43:57 -0300

    resultado IMC no JS

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ function calcular() {
     let peso = (document.getElementById("peso").value);
     let altura = (document.getElementById("altura").value);
     let resultado = document.getElementById("resultado");
+    
 }
 
 if (isNaN(peso) || isNaN(altura) || altura <= 0) {
     resultado.innerHTML = "Por favor, insira valores válido!";
-    
+    resultado.styles.color = "red";
+    return;
 }
+let imc = peso / (altura ** 2);
+
+let mensagem = `Seu IMC é: ${imc.toFixed(1)}<br>`
